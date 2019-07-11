e17ede38c9f065b6747d66c8fe52a0c200b5656e
朱晔 <ye.zhu001@BKJK.COM>
2019-07-11 13:40:08 +0800

    init

---
+++ src/test/java/me/josephzhu/javaconcurrenttest/concurrent/ThreadPoolExceptionTest.java
@@ -0,0 +1,29 @@
+package me.josephzhu.javaconcurrenttest.concurrent;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+@Slf4j
+public class ThreadPoolExceptionTest {
+    @Test
+    public void test() throws InterruptedException {
+        String prefix = "test";
+        ExecutorService threadPool = Executors.newFixedThreadPool(1, new ThreadFactoryImpl(prefix));
+        IntStream.rangeClosed(1, 10).forEach(i -> threadPool.execute(() -> {
+            if (i == 5) throw new RuntimeException("error");
+            log.info("I'm done : {}", i);
+            if (i < 5) Assert.assertEquals(prefix + "1", Thread.currentThread().getName());
+            else Assert.assertEquals(prefix + "2", Thread.currentThread().getName());
+
+        }));
+
+        threadPool.shutdown();
+        threadPool.awaitTermination(1, TimeUnit.HOURS);
+    }
+}
