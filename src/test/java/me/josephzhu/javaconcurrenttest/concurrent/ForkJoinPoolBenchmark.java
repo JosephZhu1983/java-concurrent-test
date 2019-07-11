64b341dbda84ec8472e2de4d98d0653a7515aecf
朱晔 <ye.zhu001@BKJK.COM>
2019-07-11 10:00:43 +0800

    init

---
+++ src/test/java/me/josephzhu/javaconcurrenttest/concurrent/ForkJoinPoolBenchmark.java
@@ -0,0 +1,35 @@
+package me.josephzhu.javaconcurrenttest.concurrent;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.util.StopWatch;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+@Slf4j
+public class ForkJoinPoolBenchmark {
+    @Test
+    public void benchmark() throws InterruptedException {
+        AtomicLong atomicLong = new AtomicLong();
+        StopWatch stopWatch = new StopWatch();
+        ExecutorService normal = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        ExecutorService forkjoin = Executors.newWorkStealingPool(Runtime.getRuntime().availableProcessors());
+
+        stopWatch.start("normal");
+        LongStream.rangeClosed(1, 10000000).forEach(__->normal.submit(atomicLong::incrementAndGet));
+        normal.shutdown();
+        normal.awaitTermination(1, TimeUnit.HOURS);
+        stopWatch.stop();
+        stopWatch.start("forkjoin");
+        LongStream.rangeClosed(1, 10000000).forEach(__->forkjoin.submit(atomicLong::incrementAndGet));
+        forkjoin.shutdown();
+        forkjoin.awaitTermination(1, TimeUnit.HOURS);
+        stopWatch.stop();
+        log.info("result:{}{}", stopWatch.prettyPrint(), atomicLong.get());
+    }
+}
--- src/test/java/me/josephzhu/javaconcurrenttest/concurrent/ThreadPoolExecutorTest.java
+++ src/test/java/me/josephzhu/javaconcurrenttest/concurrent/ThreadPoolExecutorTest.java
@@ -1,11 +1,11 @@
 package me.josephzhu.javaconcurrenttest.concurrent;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.IntStream;
 
 @Slf4j
@@ -14,7 +14,7 @@ public class ThreadPoolExecutorTest {
     @Test
     public void testThreadPoolSize() throws InterruptedException {
         AtomicInteger atomicInteger = new AtomicInteger();
-        BlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>(100) {
+        BlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>(20) {
             @Override
             public boolean offer(Runnable e) {
                 if (size() == 0) {
@@ -24,36 +24,61 @@ public class ThreadPoolExecutorTest {
                 }
             }
         };
-        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(2,5,5, TimeUnit.SECONDS, queue);
-        threadPool.setRejectedExecutionHandler((r, executor) -> {
+
+        class ThreadFactoryImpl implements ThreadFactory {
+            private final AtomicLong threadIndex = new AtomicLong(0);
+            private final String threadNamePrefix;
+            private final boolean daemon;
+
+            public ThreadFactoryImpl(final String threadNamePrefix) {
+                this(threadNamePrefix, false);
+            }
+
+            public ThreadFactoryImpl(final String threadNamePrefix, boolean daemon) {
+                this.threadNamePrefix = threadNamePrefix;
+                this.daemon = daemon;
+            }
+
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread thread = new Thread(r, threadNamePrefix + this.threadIndex.incrementAndGet());
+                thread.setDaemon(daemon);
+                return thread;
+            }
+        }
+
+        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(2, 5,
+                5, TimeUnit.SECONDS,
+                queue, new ThreadFactoryImpl("aa"), (r, executor) -> {
             try {
                 executor.getQueue().put(r);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
-                return;
             }
         });
+
         threadPool.allowCoreThreadTimeOut(false);
 
-        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(()->{
+
+        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(() -> {
             log.info("=========================");
             log.info("Pool Size:{}", threadPool.getPoolSize());
             log.info("Active Threads:{}", threadPool.getActiveCount());
             log.info("Number of Tasks Completed:{}", threadPool.getCompletedTaskCount());
             log.info("Totel Number of Tasks: {}", threadPool.getTaskCount());
             log.info("=========================");
-        }, 0,1, TimeUnit.SECONDS);
+        }, 0, 1, TimeUnit.SECONDS);
 
-            IntStream.rangeClosed(1,10).forEach(i-> {
+        IntStream.rangeClosed(1, 100).forEach(i -> {
 //                try {
 //                    TimeUnit.SECONDS.sleep(1);
 //                } catch (InterruptedException e) {
 //                    e.printStackTrace();
 //                }
-                int id = atomicInteger.incrementAndGet();
+            int id = atomicInteger.incrementAndGet();
             log.info("{} started", id);
 
-                threadPool.submit(() -> {
+            threadPool.submit(() -> {
                 try {
                     TimeUnit.SECONDS.sleep(10);
                 } catch (InterruptedException e) {
