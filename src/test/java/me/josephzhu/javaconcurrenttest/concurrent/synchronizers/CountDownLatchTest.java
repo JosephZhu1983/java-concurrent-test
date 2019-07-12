f04beeab1913019d3596a70e4ad94a5a2cefe06f
朱晔 <ye.zhu001@BKJK.COM>
2019-07-12 10:28:24 +0800

    init

--- src/test/java/me/josephzhu/javaconcurrenttest/concurrent/synchronizers/CountDownLatchTest.java
+++ src/test/java/me/josephzhu/javaconcurrenttest/concurrent/synchronizers/CountDownLatchTest.java
@@ -1,4 +1,41 @@
 package me.josephzhu.javaconcurrenttest.concurrent.synchronizers;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.IntStream;
+
+@Slf4j
 public class CountDownLatchTest {
+    @Test
+    public void test() throws InterruptedException {
+        int taskCount = 10;
+        CountDownLatch countDownLatch = new CountDownLatch(taskCount);
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        List<Future<Integer>> tasks = new ArrayList<>();
+        LocalDateTime begin = LocalDateTime.now();
+        IntStream.rangeClosed(1, taskCount).forEach(i -> tasks.add(threadPool.submit(() -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(i * 100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            countDownLatch.countDown();
+            return i;
+        })));
+        countDownLatch.await();
+        log.info("count:{},total:{},took:{}ms", tasks.size(), tasks.stream().map(item -> {
+            try {
+                return item.get();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return 0;
+        }).mapToInt(Integer::intValue).sum(), Duration.between(begin, LocalDateTime.now()).toMillis());
+    }
 }
--- src/test/java/me/josephzhu/javaconcurrenttest/concurrent/synchronizers/CyclicBarrierTest.java
+++ src/test/java/me/josephzhu/javaconcurrenttest/concurrent/synchronizers/CyclicBarrierTest.java
@@ -1,4 +1,40 @@
 package me.josephzhu.javaconcurrenttest.concurrent.synchronizers;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
 public class CyclicBarrierTest {
+    @Test
+    public void test() throws InterruptedException {
+
+        int taskCount = 100;
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(taskCount);
+        ExecutorService threadPool = Executors.newFixedThreadPool(100);
+        for (int j = 0; j < 10; j++) {
+            for (int i = 0; i < taskCount; i++) {
+                final String index = String.format("%d-%d", j, i);
+                threadPool.execute(() -> {
+                    atomicInteger.incrementAndGet();
+                    //log.debug("start:{}", index);
+                    try {
+                        if (cyclicBarrier.await() == 0) {
+                            log.info("done:{}", atomicInteger.getAndSet(0));
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        }
+
+        TimeUnit.SECONDS.sleep(30);
+    }
 }
