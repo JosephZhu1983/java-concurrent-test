eeda8ddbcb3972407b45b201afc6dacee951ef63
朱晔 <ye.zhu001@BKJK.COM>
2019-07-15 15:01:43 +0800

    init

--- src/test/java/me/josephzhu/javaconcurrenttest/concurrent/concurrentcollections/CopyOnWriteArrayListTest.java
+++ src/test/java/me/josephzhu/javaconcurrenttest/concurrent/concurrentcollections/CopyOnWriteArrayListTest.java
@@ -1,4 +1,49 @@
 package me.josephzhu.javaconcurrenttest.concurrent.concurrentcollections;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.util.StopWatch;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Slf4j
 public class CopyOnWriteArrayListTest {
+
+    int count = 10000000;
+
+    private void addAll(List<Integer> list) {
+        list.addAll(IntStream.rangeClosed(1, count).boxed().collect(Collectors.toList()));
+    }
+
+    @Test
+    public void test() {
+        List<Integer> copyOnWriteArrayList = new CopyOnWriteArrayList<>();
+        List<Integer> arrayList = new ArrayList<>();
+        List<Integer> synchronizedList = Collections.synchronizedList(new ArrayList<>());
+        addAll(copyOnWriteArrayList);
+        addAll(arrayList);
+        addAll(synchronizedList);
+        StopWatch stopWatch = new StopWatch();
+        int loopCount = 100000000;
+        stopWatch.start("copyOnWriteArrayList");
+        IntStream.rangeClosed(1, loopCount).parallel().forEach(__ -> copyOnWriteArrayList.get(ThreadLocalRandom.current().nextInt(count)));
+        stopWatch.stop();
+        stopWatch.start("arrayList");
+        IntStream.rangeClosed(1, loopCount).parallel().forEach(__ -> {
+            synchronized (arrayList) {
+                arrayList.get(ThreadLocalRandom.current().nextInt(count));
+            }
+        });
+        stopWatch.stop();
+        stopWatch.start("synchronizedList");
+        IntStream.range(0, loopCount).parallel().forEach(__ -> synchronizedList.get(ThreadLocalRandom.current().nextInt(count)));
+        stopWatch.stop();
+        log.info(stopWatch.prettyPrint());
+    }
 }
